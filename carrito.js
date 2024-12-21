3859e3c4865d6f4bf438c8a315c7abb8ba1e1fcc
techcristian <100042922+techcristian@users.noreply.github.com>
2024-12-21 18:00:29 -0300

    carrito de compras

---
+++ carrito.js
@@ -0,0 +1,152 @@
+/* traemos del almacenamiento local los productos agregados */
+let productosEnCarrito = localStorage.getItem("productos-en-carrito");
+productosEnCarrito = JSON.parse(productosEnCarrito);
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
+const contenedorCarritoProductos = document.querySelector("#carrito-productos");
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+const botonVaciarCarrito= document.querySelector("#vaciar-carrito");
+let subTotal=document.querySelector("#sub-total");
+let total=document.querySelector("#total");
+
+function cargarProductosCarrito(){
+  if (productosEnCarrito && productosEnCarrito.length > 0) {
+    
+    contenedorCarritoVacio.classList.add("disabled");
+    contenedorCarritoProductos.classList.remove("disabled");
+    contenedorCarritoAcciones.classList.remove("disabled");
+    contenedorCarritoComprado.classList.add("disabled");
+    
+    contenedorCarritoProductos.innerHTML="";
+    
+    productosEnCarrito.forEach(producto => {
+      const div = document.createElement("div");
+      div.classList.add("carrito-producto");
+      div.innerHTML = `
+   <img
+    class="carrito-producto-imagen"
+    src= ${producto.imagen}
+    alt=${producto.titulo}
+  />
+  <div class="items-carrito">
+    <div class="item-carrito">
+      <div class="carrito-producto-titulo">
+        <small>Titulo</small>
+        <h3>${producto.titulo}</h3>
+      </div>
+      <div class="carrito-producto-cantidad">
+        <small>Cantidad</small>
+        <p>${producto.cantidad}</p>
+      </div>
+      <div class="carrito-producto-precio">
+        <small>Precio</small>
+        <p>${producto.precio}</p>
+      </div>
+      <div class="carrito-precio-descuento">
+        <small>Precio Descuento</small>
+        <p class="carrito-precio-off">${producto.precio - producto.precio*0.10}</p>
+        <p class="carrito-descuento">
+          <span class="numero-descuento">10</span>% OFF
+        </p>
+      </div>
+      <div class="carrito-producto-subtotal">
+        <small>Subtotal</small>
+        <p>${producto.precio - producto.precio*0.10}</p>
+      </div>
+    </div>
+    <button class="carrito-producto-eliminar" id=${producto.id}>
+      <i class="fa-solid fa-trash"></i>Eliminar
+    </button>
+  </div>
+      `;
+      contenedorCarritoProductos.append(div);
+  
+    });
+  } else {
+    contenedorCarritoVacio.classList.remove("disabled");
+    contenedorCarritoProductos.classList.add("disabled");
+    contenedorCarritoAcciones.classList.add("disabled");
+    contenedorCarritoComprado.classList.add("disabled");
+  };
+  actualizarBotonesEliminar();
+  actualizarsubTotal()
+  actualizarTotal()
+};
+
+cargarProductosCarrito();
+function actualizarBotonesEliminar(){
+  botonesEliminar= document.querySelectorAll(".carrito-producto-eliminar");
+  botonesEliminar.forEach(boton=>{
+    boton.addEventListener("click", eliminarCarrito);
+  })
+
+};
+
+function eliminarCarrito(e){
+  const idBoton = e.currentTarget.id;
+  const index= productosEnCarrito.findIndex(producto => producto.id === idBoton);
+  productosEnCarrito.splice(index,1);
+  cargarProductosCarrito();
+
+  localStorage.setItem("productos-en-carrito",JSON.stringify(productosEnCarrito));
+};
+
+botonVaciarCarrito.addEventListener("click",vaciarCarrito);
+function vaciarCarrito(){
+  productosEnCarrito.length=0;
+  localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+  cargarProductosCarrito();
+};
+
+function actualizarsubTotal(){
+  subTotal.innerText= productosEnCarrito.reduce((acc,producto)=> acc+(producto.precio-producto.precio*0.10 * producto.cantidad),0);
+  
+}
+function actualizarTotal(){
+  total.innerText= productosEnCarrito.reduce((acc,producto)=> acc+(producto.precio-producto.precio*0.10 * producto.cantidad),0);
+  
+}
+
+
+
+
+
+
+/*<div class="carrito-producto">
+<img
+  class="carrito-producto-imagen"
+  src="imgs/reloj-rolex1.jpg"
+  alt="carrito-01"
+/>
+<div class="items-carrito">
+  <div class="item-carrito">
+    <div class="carrito-producto-titulo">
+      <small>Titulo</small>
+      <h3>Rolex 01</h3>
+    </div>
+    <div class="carrito-producto-cantidad">
+      <small>Cantidad</small>
+      <p>1</p>
+    </div>
+    <div class="carrito-producto-precio">
+      <small>Precio</small>
+      <p>$75000</p>
+    </div>
+    <div class="carrito-precio-descuento">
+      <small>Precio Descuento</small>
+      <p class="carrito-precio-off">$67.500</p>
+      <p class="carrito-descuento">
+        <span class="numero-descuento">10</span>% OFF
+      </p>
+    </div>
+    <div class="carrito-producto-subtotal">
+      <small>Subtotal</small>
+      <p>$67500</p>
+    </div>
+  </div>
+  <button class="carrito-producto-eliminar">
+    <i class="fa-solid fa-trash"></i>Eliminar
+  </button>
+</div>
+</div>*/
---
+++ index.js
@@ -0,0 +1,224 @@
+/* Creacion de un array con productos */
+const productos = [
+  {
+    id: "Rolex-01",
+    titulo: "Rolex 01",
+    imagen: "imgs/reloj-rolex1.jpg",
+    categoria: {
+      nombre: "Rolex",
+      id: "Rolex"
+    },
+    precio: 75000
+  },
+  {
+    id: "Rolex-02",
+    titulo: "Rolex 02",
+    imagen: "imgs/reloj-rolex2.webp",
+    categoria: {
+      nombre: "Rolex",
+      id: "Rolex"
+    },
+    precio: 90000
+  },
+  {
+    id: "Rolex-03",
+    titulo: "Rolex 03",
+    imagen: "imgs/reloj-rolex3.webp",
+    categoria: {
+      nombre: "Rolex",
+      id: "Rolex"
+    },
+    precio: 120000
+  },
+  {
+    id: "Casio-01",
+    titulo: "Casio 01",
+    imagen: "imgs/reloj-casio1.webp",
+    categoria: {
+      nombre: "Casio",
+      id: "Casio"
+    },
+    precio: 750000
+  },
+  {
+    id: "Casio-02",
+    titulo: "Casio 02",
+    imagen: "imgs/reloj-casio2.webp",
+    categoria: {
+      nombre: "Casio",
+      id: "Casio"
+    },
+    precio: 90000
+  },
+  {
+    id: "Casio-03",
+    titulo: "Casio 03",
+    imagen: "imgs/reloj-casio3.webp",
+    categoria: {
+      nombre: "Casio",
+      id: "Casio"
+    },
+    precio: 120000
+  },
+  {
+    id: "Zenith-01",
+    titulo: "Zenith 01",
+    imagen: "imgs/reloj-zenith1.webp",
+    categoria: {
+      nombre: "Zenith",
+      id: "Zenith"
+    },
+    precio: 75000
+  },
+  {
+    id: "Zenith-02",
+    titulo: "Zenith 02",
+    imagen: "imgs/reloj-zenith2.webp",
+    categoria: {
+      nombre: "Zenith",
+      id: "Zenith"
+    },
+    precio: 90000
+  },
+  {
+    id: "Zenith-03",
+    titulo: "Zenith 03",
+    imagen: "imgs/reloj-zenith3.webp",
+    categoria: {
+      nombre: "Zenith",
+      id: "Zenith"
+    },
+    precio: 120000
+  },
+  {
+    id: "Seiko-01",
+    titulo: "Seiko 01",
+    imagen: "imgs/reloj-seiko1.webp",
+    categoria: {
+      nombre: "Seiko",
+      id: "Seiko"
+    },
+    precio: 75000
+  },
+  {
+    id: "Seiko-02",
+    titulo: "Seiko 02",
+    imagen: "imgs/reloj-seiko2.webp",
+    categoria: {
+      nombre: "Seiko",
+      id: "Seiko"
+    },
+    precio: 90000
+  },
+  {
+    id: "Seiko-03",
+    titulo: "Seiko 03",
+    imagen: "imgs/reloj-seiko3.webp",
+    categoria: {
+      nombre: "Seiko",
+      id: "Seiko"
+    },
+    precio: 120000
+  }
+];
+/*variables desde el DOM*/
+const contenedorProductos = document.querySelector("#contenedor-productos")
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar;
+const numerito = document.querySelector("#numerito");
+
+/*funciones*/
+/*funcion que crea cada tarjeta de productos*/
+function cargarProductos(productosElegidos) {
+  contenedorProductos.innerHTML = "";
+  productosElegidos.forEach(producto => {
+    const div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `
+      <img
+              class="producto-imagen"
+              src="${producto.imagen}"
+              alt="${producto.titulo}"
+            />
+            <div class="producto-detalles">
+              <h4 class="producto-titulo">${producto.titulo}</h4>
+              <div class="contenedor-descuento">
+                <p class="producto-precio">${producto.precio}</p>
+                <p class="descuento">
+                  <span class="numero-descuento">10</span>% OFF
+                </p>
+              </div>
+              <p class="precio-off">${producto.precio- producto.precio*0.10}</p>
+              <button class="producto-agregar" type="submit" id=${producto.id}>
+                Agregar carrito
+              </button>
+           
+            </div>
+    `;
+    contenedorProductos.append(div);
+  });
+  actualizarBotonesAgregar();
+};
+
+/*cargamos todos los productos*/
+cargarProductos(productos);
+
+/* recorremos cada boton del aside de la sectcion productos y filtramos por categoria*/
+botonesCategorias.forEach(boton => {
+  boton.addEventListener("click", (e) => {
+    botonesCategorias.forEach(boton => { boton.classList.remove("active") })
+    e.currentTarget.classList.add("active");
+    if (e.currentTarget.id != "todos") {
+      const tituloProducto = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+      tituloPrincipal.innerText = tituloProducto.categoria.nombre;
+      const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+      cargarProductos(productosBoton);
+    } else {
+      tituloPrincipal.innerText = "Todos los relojes";
+      cargarProductos(productos);
+    }
+
+  });
+
+})
+/*al seleccionar cada boton de categoria el boton de agregar se vuelve a crear en la funcion cargarProductos(), por eso debemos volver a obtener la nueva clase (agregar-producto) de cada boton y en el cual podemos aplicar el evento "click". */
+function actualizarBotonesAgregar() {
+  botonesAgregar = document.querySelectorAll(".producto-agregar");
+  botonesAgregar.forEach(boton => {
+    boton.addEventListener("click", agregarAlCarrito);
+  })
+};
+/*funcion que agrega a un array el producto seleccionado con el evento, buscandolo en el array de productos y comparando su id de producto.*/
+let productosEnCarrito;
+let productosEnCarritoLs = localStorage.getItem("productos-en-carrito");
+
+
+if (productosEnCarritoLs) {
+  productosEnCarrito = JSON.parse(productosEnCarritoLs);
+  actualizarNumerito();
+} else {
+  productosEnCarrito = [];
+}
+function agregarAlCarrito(e) {
+  const idBoton = e.currentTarget.id;
+  const productoAgregado = productos.find(producto => producto.id === idBoton);
+  if (productosEnCarrito.some(producto => producto.id === idBoton)) {
+    const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+    productosEnCarrito[index].cantidad++;
+
+  } else {
+    productoAgregado.cantidad = 1;
+    productosEnCarrito.push(productoAgregado);
+  }
+  actualizarNumerito();
+  localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+};
+
+/*Sumar Productos en el carrito */
+function actualizarNumerito() {
+ let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+  numerito.innerText = nuevoNumerito;
+}
+
+/*   <button class="producto-comprar" type="submit" id=${producto.id}>Comprar</button>*/
